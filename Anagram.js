c0d05178edff47375a57108d41413a90558170ee
Aayushi Solanki <36412316+aayushisolanki28@users.noreply.github.com>
2024-02-09 16:29:11 +0530

    Create Anagram.js

---
+++ Anagram.js
@@ -0,0 +1,27 @@
+//String Anagram
+//condition
+//Check length for both strings
+// string will have same set of letters
+
+function isAnagram(string1,string2){
+   
+    if(string1.length != string2.length){
+        return false
+    }
+
+    let counter = {}
+        for(let letter of string1){
+            counter[letter] = (counter[letter] || 0) + 1     
+    }
+    // console.log(counter)
+    for (let item of string2){
+        if(!counter[item]){
+            return false
+        }
+        counter[item] -= 1
+    }
+    return true
+
+}
+
+console.log(isAnagram('hello','llhoe'))
